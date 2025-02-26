803827905363627d9b33cd2f4e10ae315313f727
Sherine Awad <sherinem@gl3166.arc-ts.umich.edu>
2025-02-26 10:51:49 -0500

    update cellratio

--- cellRatio.py
+++ cellRatio.py
@@ -14,27 +14,13 @@ myObject =  args.myObject
 
 combined_adata = sc.read_h5ad(myObject, backed="r")
 
-# Step 1: Count the total number of cells for each cell type
 cell_type_counts = combined_adata.obs['celltype'].value_counts()
 
-# Step 2: Count the number of cells per cell type and cluster
-cluster_and_celltype_counts = combined_adata.obs.groupby(['celltype', 'leiden']).size()
+total_cells = cell_type_counts.sum()
 
-# Step 3: Reshape the counts to a DataFrame (clusters as columns)
-cluster_and_celltype_counts_unstacked = cluster_and_celltype_counts.unstack(fill_value=0)
+cell_type_ratios = cell_type_counts / total_cells
 
-# Step 4: Calculate the proportion of cells in each cluster for each cell type
-# We will divide the counts for each cluster by the total number of cells in that cell type
-cell_type_proportions = cluster_and_celltype_counts_unstacked.div(cell_type_counts, axis=0)
+print(cell_type_ratios)
 
-# Step 5: Print the result
-print("Cluster Proportions per Cell Type:")
-print(cell_type_proportions)
-
-
-
-
-
- 
 
 
